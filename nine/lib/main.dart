1bfeac98e879f62b5045c7242bbb85694f243759
saitamaaaa <shresthasarika06@gmail.com>
2021-08-30 18:54:21 +0545

    message

--- nine/lib/main.dart
+++ nine/lib/main.dart
@@ -1,189 +1,217 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'dating app',
-    home: home(),
+    title: 'loginPage',
+    home: loginPage(),
   ));
 }
 
-class home extends StatelessWidget {
+class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          color: Colors.black,
-        
-    child:  Column(
+      backgroundColor: Color(0xff050709),
+      appBar: AppBar(
+        elevation: 0.0,
+        toolbarHeight: 30,
+        backgroundColor: Color(0xff050709),
+        title: const Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {},
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-            Expanded(
-              child: ListView(
-            padding: const EdgeInsets.all(50),
-                children:<Widget>[
-                
-                   Container(
-                      color: Colors.yellow,
-                      height: 50.0, 
-                    ),
-                  
                   SizedBox(
-                    height: 10.0,
+                    height: 20,
                   ),
-                  Container(
-                    color: Colors.yellow,
-                    height: 50.0,
+                  Text(
+                    "Welcome back!",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
                   ),
-                   SizedBox(
-                    height: 10.0,
+                  SizedBox(
+                    height: 6,
                   ),
-                  Container(
-                    color: Colors.yellow,
-                    height: 50.0,
+                  Text(
+                    "You have been missed.",
+                    style: TextStyle(fontSize: 15, color: Colors.grey.shade400),
                   ),
-                   SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    height: 50.0,
-                  ),
-                   SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 50.0,
+                  SizedBox(
+                    height: 5,
                   ),
                 ],
               ),
-          
-            ),
-             SizedBox(
-                    height: 5.0,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    color: Color(0xff262626),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey.shade400,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            )),
+                      ),
+                    ),
                   ),
-            Expanded(child: 
-            ListView(
-            padding: const EdgeInsets.all(50),
-               scrollDirection: Axis.horizontal,
-        
-             
-              children: <Widget> [
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            
-                children:<Widget>[
-                
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    color: Color(0xff262626),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey.shade400,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade300,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                            )),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  loginButton(),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 80.0,
-                      width: 80.0,
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.orange),
+                        ),
+                      ],
                     ),
                   ),
-     Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 80.0,
-                      width: 80.0,
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "OR",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.grey.shade400,
+                      ),
                     ),
-                  ),
-                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 80.0,
-                      width: 80.0,
-                    ),
-                  ),
-                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 80.0,
-                      width: 80.0,
-                    ),
-                  ),
-                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 80.0,
-                      width: 80.0,
-                    ),
-                   ),
-                 
-                  
-                 
-                ],
-              )
-              ],
-            ),),
-            
-            Expanded(child: ListView(
-               padding: const EdgeInsets.all(50),
-                   children:<Widget> [
-             Container(  
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                       color: Colors.red,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ), 
-                   SizedBox(
-                    height: 10,
-                  ), 
-                  Container(
-                    color: Colors.red,
-                    height: 50.0,
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 20,
                   ),
-                  Container(
-                    color: Colors.red,
-                    height: 50.0,
+                  googl(),
+                  SizedBox(
+                    height: 180,
                   ),
-                   SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    height: 50.0,
-                  ),
-                   SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    height: 50.0,
-                  ),
-               SizedBox(
-                    height: 10,
-                  ),
-                  Container(  
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                       color: Colors.red,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ), 
-                 
                 ],
-            
-            ),),
-                  
-                ],
-              ) ,
               ),
-            
-            
-        
-          
-        
-        );
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class loginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Color(0xffFBBA1A),
+      height: 50.0,
+      minWidth: 500.0,
+      child: Text(
+        "Login",
+        style: TextStyle(
+          color: Color(0xff050709),
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
+}
+
+class googl extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Color(0xff518DF9),
+      height: 50.0,
+      minWidth: 500.0,
+      child: Text(
+        "Connect with Google",
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
   }
 }
